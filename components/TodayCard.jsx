c5a6178545976b8c49023ed69a1be04eb7c3369a
Cenk Bülbül <cenkbulbul67@gmail.com>
2024-11-11 22:04:01 +0300

    fix: today cart updated

--- components/TodayCard.jsx
+++ components/TodayCard.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
+
 import { TiWeatherPartlySunny } from "react-icons/ti";
+import { MdOutlineTaskAlt } from "react-icons/md";
 
 const TodayCard = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
@@ -20,16 +22,64 @@ const TodayCard = () => {
   const dateString = currentTime.toLocaleDateString();
 
   return (
-    <div className="flex flex-col gap-3 justify-between bg-white p-5 rounded-2xl max-w-[300px] font-light">
+    <div className="flex flex-col gap-8 bg-white p-5 rounded-2xl font-light">
       <div className="flex gap-2 items-center">
         <TiWeatherPartlySunny className="text-5xl" />
-        <div className="flex flex-col gap-1">
-          <span>{timeString}</span>
+        <div>
+          <div className="flex gap-2">
+            <span>
+              <span className="text-xs">Today:</span>
+              {dateString}
+            </span>
+            <span>{timeString}</span>
+          </div>
           <span className="text-xs">Realtime Insight</span>
         </div>
       </div>
-      <div>
-        Today: <span className="font-bold">{dateString}</span>
+
+      <div className="flex flex-col gap-2">
+        <div className="text-sm underline underline-offset-4 ">
+          Today&apos;s events
+        </div>
+
+        <ul className="text-sm flex flex-col gap-2 h-[100px] overflow-auto ">
+          <li className="flex items-center gap-2">
+            <MdOutlineTaskAlt className="text-blue-700" />
+            <span>18.30 hr toplantı</span>
+          </li>
+          <li className="flex items-center gap-2">
+            <MdOutlineTaskAlt className="text-blue-700" />
+            <span>Stajyerlerle görüşme</span>
+          </li>
+          <li className="flex items-center gap-2">
+            <MdOutlineTaskAlt className="text-blue-700" />
+            <span>Frontend İş Görüşmesi</span>
+          </li>
+          <li className="flex items-center gap-2">
+            <MdOutlineTaskAlt className="text-blue-700" />
+            <span>18.30 hr toplantı</span>
+          </li>
+          <li className="flex items-center gap-2">
+            <MdOutlineTaskAlt className="text-blue-700" />
+            <span>Stajyerlerle görüşme</span>
+          </li>
+          <li className="flex items-center gap-2">
+            <MdOutlineTaskAlt className="text-blue-700" />
+            <span>Frontend İş Görüşmesi</span>
+          </li>
+          <li className="flex items-center gap-2">
+            <MdOutlineTaskAlt className="text-blue-700" />
+            <span>18.30 hr toplantı</span>
+          </li>
+          <li className="flex items-center gap-2">
+            <MdOutlineTaskAlt className="text-blue-700" />
+            <span>Stajyerlerle görüşme</span>
+          </li>
+          <li className="flex items-center gap-2">
+            <MdOutlineTaskAlt className="text-blue-700" />
+            <span>Frontend İş Görüşmesi</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
